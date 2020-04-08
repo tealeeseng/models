45f470ad6aa5fd7585bd8fe693af93378474bd51
tealeeseng <tealeeseng@gmail.com>
2020-04-08 16:01:08 +0800

    Training scripts

--- research/safety-larger-training.sh
+++ research/safety-larger-training.sh
@@ -2,8 +2,8 @@ export CUDA_VISIBLE_DEVICES="0"
 
 
 python3 object_detection/model_main.py \
-    --pipeline_config_path=object_detection/safety-loads/safety-pipeline.config \
-    --model_dir=object_detection/safety-loads/models \
+    --pipeline_config_path=object_detection/safety-large-loads/safety-larger-pipeline.config \
+    --model_dir=object_detection/safety-large-loads/models \
     --num_train_steps=10000 \
     --alsologtostderr
 
